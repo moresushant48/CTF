f513c484b0f84a8cf71195ee38a4489ff0b35a6e
Sushant More <moresushant48@gmail.com>
2021-07-30 16:11:21 +0530

    Component : Add NavBar Component
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './modules/authentication/authentication.module';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
