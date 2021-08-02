2a4bda6d33058610c4c5ea8eac9f74f75b902afc
Sushant More <moresushant48@gmail.com>
2021-08-02 15:13:52 +0530

    Component: Add Footer Component.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,11 +20,13 @@ import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
 import { MdbTooltipModule } from 'mdb-angular-ui-kit/tooltip';
 import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
